9320b4f65114d6829a34acb81e839d9c233deaeb
GregoryRobetertson <geefromla@gmail.com>
2024-05-03 22:57:01 -0500

    updated

--- src/app/DataDisplay.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import React from 'react'
-
-export default function DataDisplay() {
-    
-  return (
-    <div>DataDisplay</div>
-  )
-}
--- src/app/SimpleForm.jsx
+++ src/app/SimpleForm.jsx
@@ -30,24 +30,51 @@ export default function SimpleForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ backgroundColor: "grey", color: "black", padding: "20px" }}>
-      <label htmlFor="name" style={{ marginBottom: "10px" }}>
-        Name:
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} style={{ marginLeft: "10px", padding: "10px" }} />
-      </label>
-      <label htmlFor="message" style={{ display: "block", marginBottom: "10px" }}>
-        Message:
-        <textarea
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          style={{ marginLeft: "10px", marginTop: "5px", padding: "10px" }}
-        ></textarea>
-      </label>
-      <button type="submit" style={{ backgroundColor: "black", color: "white", padding: "10px 20px", border: "none", borderRadius: "5px", cursor: "pointer" }}>Add Data</button>
-    </form>
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        style={{ backgroundColor: "grey", color: "black", padding: "20px" }}
+      >
+        <label htmlFor="name" style={{ marginBottom: "10px" }}>
+          Name:
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginLeft: "10px", padding: "10px" }}
+          />
+        </label>
+        <label
+          htmlFor="message"
+          style={{ display: "block", marginBottom: "10px" }}
+        >
+          Message:
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            style={{ marginLeft: "10px", marginTop: "5px", padding: "10px" }}
+          ></textarea>
+        </label>
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Add Data
+        </button>
+      </form>
+      <div></div>
+    </div>
   );
 }
