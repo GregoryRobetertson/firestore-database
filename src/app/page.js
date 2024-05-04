ae68cc7610d43100dca6fe66f856f85c036e293c
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-05-04 05:05:04 +0000

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import DataDisplay from "./DataDisplay";
 import SimpleForm from "./SimpleForm";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SimpleForm/>
+      <SimpleForm />
+      <DataDisplay/>
     </main>
   );
 }
